34e790cd09d7272567e3f177ded3be184246a3e8
Adrian Slimak <adek124pl@gmail.com>
2023-09-05 11:54:51 -0700

    Update

--- terraform/cluster/cluster.tf
+++ terraform/cluster/cluster.tf
@@ -61,10 +61,10 @@ resource "aws_key_pair" "cluster_keypair" {
 
   # generate "key.pem" in current directory
   provisioner "local-exec" {
-    interpreter = ["powershell", "-Command"]
+    # interpreter = ["powershell", "-Command"]
     command = <<-eot
       echo '${tls_private_key.cluster_key.private_key_pem}' > ./'${var.project_name}_key'.pem
-      # chmod 400 ./'${var.project_name}_key'.pem
+      chmod 400 ./'${var.project_name}_key'.pem
     eot
   }
 }
@@ -108,6 +108,8 @@ data "cloudinit_config" "master_cloud_init" {
     content_type = "text/x-shellscript"
     content      = templatefile("${path.module}/scripts/init_kubernetes_master.sh", {
       cluster_name = "${var.project_name}_cluster",
+      dns_name = "${var.project_name}.${var.aws_region}",
+      ip_address = aws_eip.master_eip.public_ip,
       kubeadm_token = module.kubeadm-token.token
     })
     # content      = templatefile("${path.module}/scripts/init_kubernetes_master.sh", { kubeadm_token = module.kubeadm-token.token, dns_name = "${var.cluster_name}.${var.hosted_zone}", ip_address = aws_eip.master.public_ip, cluster_name = var.cluster_name, addons = join(" ", var.addons), aws_region = var.aws_region, asg_name = "${var.cluster_name}-nodes", asg_min_nodes = var.min_worker_count, asg_max_nodes = var.max_worker_count, aws_subnets = join(" ", concat(var.worker_subnet_ids, [var.master_subnet_id])) } )
@@ -116,9 +118,8 @@ data "cloudinit_config" "master_cloud_init" {
 
 # EC2 Instances - Master Nodes
 
-# resource "aws_eip" "master_eip" {
-#   vpc = true
-# }
+resource "aws_eip" "master_eip" {
+}
 
 resource "aws_instance" "master_instance" {
   ami = data.aws_ami.amazon_linux.id
@@ -127,7 +128,7 @@ resource "aws_instance" "master_instance" {
   key_name = aws_key_pair.cluster_keypair.key_name
 
   subnet_id = data.aws_subnet.master_subnet.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   vpc_security_group_ids = [
     data.aws_security_group.sg.id
@@ -151,14 +152,14 @@ resource "aws_instance" "master_instance" {
   }
 }
 
-output "master_instance_public_ip" {
-  value = ["${aws_instance.master_instance.public_ip}"]
+resource "aws_eip_association" "master_eip_assoc" {
+  instance_id   = aws_instance.master_instance.id
+  allocation_id = aws_eip.master_eip.id
 }
 
-# resource "aws_eip_association" "master_eip_assoc" {
-#   instance_id   = aws_instance.master_instance.id
-#   allocation_id = aws_eip.master_eip.id
-# }
+output "master_instance_public_ip" {
+  value = ["${aws_eip.master_eip.public_ip}"]
+}
 
 # EC2 Instances - Worker Nodes
 
--- terraform/cluster/scripts/init_kubernetes_master.sh
+++ terraform/cluster/scripts/init_kubernetes_master.sh
@@ -8,11 +8,17 @@ set -o errexit
 set -o pipefail
 
 export CLUSTER_NAME=${cluster_name}
+export DNS_NAME=${dns_name}
+export IP_ADDRESS=${ip_address}
 export KUBERNETES_VERSION="1.28.0"
 export KUBEADM_TOKEN=${kubeadm_token}
 
 # Set this only after setting the defaults
 set -o nounset
+
+# We needed to match the hostname expected by kubeadm an the hostname used by kubelet
+LOCAL_IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+FULL_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/hostname)"
 
 # Disable SELinux
 # setenforce returns non zero if already SE Linux is already disabled
@@ -62,9 +68,107 @@ gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
 EOF
 yum install containerd.io -y
+
 mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 systemctl restart containerd
 systemctl enable containerd
+
+###
+# Install Kubernetes
+###
+
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+yum install -y kubelet-$KUBERNETES_VERSION kubeadm-$KUBERNETES_VERSION kubernetes-cni --disableexcludes=kubernetes
+
+# Start services
+systemctl enable kubelet
+systemctl start kubelet
+
+# Fix certificates file on CentOS
+if cat /etc/*release | grep ^NAME= | grep CentOS ; then
+    rm -rf /etc/ssl/certs/ca-certificates.crt/
+    cp /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
+fi
+
+###
+# Initialize Kubernetes cluster
+###
+
+cat >/tmp/kubeadm.yaml <<EOF
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: InitConfiguration
+bootstrapTokens:
+  - groups:
+      - system:bootstrappers:kubeadm:default-node-token
+    token: $KUBEADM_TOKEN
+    ttl: 0s
+    usages:
+      - signing
+      - authentication
+nodeRegistration:
+  criSocket: unix:///var/run/containerd/containerd.sock
+  imagePullPolicy: IfNotPresent
+  kubeletExtraArgs:
+    cgroup-driver: systemd
+    cloud-provider: aws
+    read-only-port: "10255"
+  name: $FULL_HOSTNAME
+  taints:
+    - effect: NoSchedule
+      key: node-role.kubernetes.io/master
+localAPIEndpoint:
+  advertiseAddress: $LOCAL_IP_ADDRESS
+  bindPort: 6443
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: ClusterConfiguration
+apiServer:
+  certSANs:
+    - $DNS_NAME
+    - $IP_ADDRESS
+    - $LOCAL_IP_ADDRESS
+    - $FULL_HOSTNAME
+  extraArgs:
+    cloud-provider: aws
+  timeoutForControlPlane: 5m0s
+certificatesDir: /etc/kubernetes/pki
+clusterName: kubernetes
+controllerManager:
+  extraArgs:
+    cloud-provider: aws
+dns: {}
+etcd:
+  local:
+    dataDir: /var/lib/etcd
+kubernetesVersion: $KUBERNETES_VERSION
+networking:
+  dnsDomain: cluster.local
+  podSubnet: 192.168.0.0/16
+  serviceSubnet: 10.96.0.0/12
+scheduler: {}
+---
+EOF
+
+kubeadm reset --force
+kubeadm init --config /tmp/kubeadm.yaml
+
+# Use the local kubectl config for further kubectl operations
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Install calico
+kubectl create -f https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/calico/calico-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/calico/calico-cr.yaml
